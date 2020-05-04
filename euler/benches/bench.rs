fa3b848c6cfdd141e1d591d83ca9a6a27ee91f56
Magnus Ulimoen <flymagnus@gmail.com>
2020-05-04 20:03:46 +0200

    fix euler bench

--- euler/benches/bench.rs
+++ euler/benches/bench.rs
@@ -1,5 +1,5 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use sbp::euler::System;
+use euler::System;
 use sbp::operators::{SbpOperator2d, Upwind4, UpwindOperator2d, SBP4};
 use sbp::Float;
 
